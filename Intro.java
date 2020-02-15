2d742c6fdb990e4a6e92a4b01db641ac59c4b4c8
Stefano-Iannone-Peano-4A <56471045+Stefano-Iannone-Peano-4A@users.noreply.github.com>
2020-02-15 09:53:48 +0100

    FASE 2
    Continuazione lavoro SpaceInvaders

--- Intro.java
+++ Intro.java
@@ -14,20 +14,27 @@ import java.awt.Graphics;
  * @author longhitano.stefano
  */
 public class Intro extends Space {
+    private int i = 0;
+    
+    void run() throws InterruptedException {
+       setBackground (Color.BLACK);
+       setSize(800, 600);
+               
+        for(i=100;i>=0;i-=1) {
+            System.out.println("i: " + i);
+            this.repaint();
+            Thread.sleep(50);
+        }
 
+    }
+    
     @Override
-    public void paint(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.setColor(Color.blue);
-        g.fillRect(0, 0, 800, 600);
-        g.setFont(new Font("Bold", Font.PLAIN, 40));
-        g.drawString("Space Invaders", 100, 100);
-        g.setFont(new Font("Bold", Font.PLAIN, 10));
-        g.drawString("          by Stefano Iannone", 110 , 110);
+   public void paint(Graphics g) {
+        // scrivi nel canvas la scritta in Rosso 
+        g.setColor(Color.red);
+        g.setFont(new Font("Bold", Font.PLAIN, 40+i));
+        g.drawString("Space Invaders", 100+i, 100+i);
+        g.setFont(new Font("Bold", Font.PLAIN, 10+i));
+        g.drawString("          by Stefano Iannone", 110+i+i, 110+i+i);
     }
-
-    void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
 }
